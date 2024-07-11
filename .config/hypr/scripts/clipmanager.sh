73a99fe73f86b3dbf7a6a482553b713e48b1048e
ifeelsam <sanskarsharma9005@gmail.com>
2024-07-11 20:22:06 +0530

    add dots

---
+++ .config/hypr/scripts/clipmanager.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+while true; do
+	result=$(
+		rofi -dmenu -display-columns 2 \
+			-kb-custom-1 "Control-Delete" \
+			-kb-custom-2 "Alt-Delete" \
+			-config ~/.config/rofi/launchers/mac/clipboard.rasi < <(cliphist list)
+	)
+
+	case "$?" in
+	1)
+		exit
+		;;
+	0)
+		case "$result" in
+		"")
+			continue
+			;;
+		*)
+			cliphist decode <<<"$result" | wl-copy
+			exit
+			;;
+		esac
+		;;
+	10)
+		cliphist delete <<<"$result"
+		;;
+	11)
+		cliphist wipe
+		;;
+	esac
+done
